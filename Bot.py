2eab23cb5b31479b3257fe8a74487245692a7d90
Pandavid0202 <acevedojuancr7@gmail.com>
2025-01-07 12:42:10 -0500

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,35 @@
+import discord
+from bot_logic import gen_pass
+from discord.ext import commands
+
+
+# La variable intents almacena los privilegios del bot
+intents = discord.Intents.default()
+# Activar el privilegio de lectura de mensajes
+intents.message_content = True
+# Crear un bot en la variable cliente y transferirle los privilegios
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("Hola")
+    
+@bot.command()
+async def bye(ctx):
+    await ctx.send("adios")
+
+@bot.command()
+async def password(ctx):
+    await ctx.send(gen_pass(10))
+
+@bot.command()
+async def repeat(ctx, times: int, content='WAA'):
+    for i in range(times):
+        await ctx.send(content)
+    
+
+bot.run("")
